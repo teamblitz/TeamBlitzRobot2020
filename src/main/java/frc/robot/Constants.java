b34a859d1464107b08b2aeda414c859fa594a392
Rob Christie <robchristie@gmail.com>
2020-02-01 12:11:39 -0700

    Add DriveConstants class.

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -8,45 +8,49 @@
 package frc.robot;
 
 /**
- * The Constants class provides a convenient place for teams to hold robot-wide numerical or boolean
- * constants.  This class should not be used for any other purpose.  All constants should be
- * declared globally (i.e. public static).  Do not put anything functional in this class.
- *
- * <p>It is advised to statically import this class (or one of its inner classes) wherever the
- * constants are needed, to reduce verbosity.
- */
+* The Constants class provides a convenient place for teams to hold robot-wide numerical or boolean
+* constants.  This class should not be used for any other purpose.  All constants should be
+* declared globally (i.e. public static).  Do not put anything functional in this class.
+*
+* <p>It is advised to statically import this class (or one of its inner classes) wherever the
+* constants are needed, to reduce verbosity.
+*/
 public final class Constants {
     public static final class ShooterConstants {
         public static final int[] kEncoderPorts = new int[]{2, 3};
         public static final boolean kEncoderReversed = false;
         public static final int kEncoderCPR = 1024;
         public static final double kEncoderDistancePerPulse =
-            // Distance units will be rotations
-            1.0 / (double) kEncoderCPR;
-
+        // Distance units will be rotations
+        1.0 / (double) kEncoderCPR;
+        
         public static final int kShooterMotorTopPort = 2;
         public static final int kShooterMotorBottomPort = 3;
-
+        
         
         public static final double kShooterFreeRPS = 5300;
         public static final double kShooterTargetRPS = 4000;
         public static final double kShooterToleranceRPS = 50;
-
+        
         // These are not real PID gains, and will have to be tuned for your specific robot.
         public static final double kP = 1;
         public static final double kI = 0;
         public static final double kD = 0;
-
+        
         // On a real robot the feedforward constants should be empirically determined; these are
         // reasonable guesses.
         public static final double kSVolts = 0.05;
         public static final double kVVoltSecondsPerRotation =
-            // Should have value 12V at free speed...
-            12.0 / kShooterFreeRPS;
-
-      }
-
-      public static final class OIConstants {
-          public static final int kDriveControllerPort = 0;
-      }
+        // Should have value 12V at free speed...
+        12.0 / kShooterFreeRPS;
+        
     }
+    
+    public static final class OIConstants {
+        public static final int kDriveControllerPort = 0;
+    }
+
+    public static final class DriveConstants {
+        
+    }
+}
